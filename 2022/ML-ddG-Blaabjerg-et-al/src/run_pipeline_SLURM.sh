fff6f0222015c0ae6b24bac27a2f61c61ea13081
Lasse Blaabjerg <32910436+lblaabjerg@users.noreply.github.com>
2022-08-16 12:01:40 +0200

    Delete run_pipeline_SLURM.sh

--- 2022/ML-ddG-Blaabjerg-et-al/src/run_pipeline_SLURM.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=cavity-model
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:1
-#SBATCH --time=120:00:00
-#SBATCH --partition=sbinlab_gpu
-#SBATCH --mem=20G
-
-python3 -u run_pipeline.py
