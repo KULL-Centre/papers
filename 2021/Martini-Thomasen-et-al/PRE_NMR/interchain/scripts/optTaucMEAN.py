882452aefcf920d99a805724c0439b825485dc75
Giulio Tesei <giu.tesei@gmail.com>
2024-01-04 16:48:31 +0100

    Update optTaucMEAN.py

--- 2021/Martini-Thomasen-et-al/PRE_NMR/interchain/scripts/optTaucMEAN.py
+++ 2021/Martini-Thomasen-et-al/PRE_NMR/interchain/scripts/optTaucMEAN.py
@@ -72,6 +72,8 @@ def optTauC(prot,model,chi2_results,rs_results,Nreplicas,tau_c_range):
                 gamma_2 = Operations.calc_gamma_2(data['r6'], s_pre, tau_c = tc * 1e-9, tau_t = 1e-10, wh = prot.wh, k = 1.23e16)
                 gamma_2 = np.ma.MaskedArray(gamma_2, mask = np.isnan(gamma_2))
                 gamma_2_av[measured_resnums] = np.ma.average(gamma_2, axis=0).data
+                all_gamma_2.append(gamma_2_av)
+
                 x,y = np.loadtxt(prot.path+model+'/run{:d}/calcPREs/resBA-{:d}.dat'.format(run,label),usecols=(0,1),unpack=True)
                 measured_resnums = np.where(~np.isnan(y))[0]
                 data = pd.read_pickle(prot.path+model+'/run{:d}/calcPREs/resBA-{:d}.pkl'.format(run,label), compression='gzip')
@@ -80,8 +82,8 @@ def optTauC(prot,model,chi2_results,rs_results,Nreplicas,tau_c_range):
                 gamma_2 = Operations.calc_gamma_2(data['r6'], s_pre, tau_c = tc * 1e-9, tau_t = 1e-10, wh = prot.wh, k = 1.23e16)
                 gamma_2 = np.ma.MaskedArray(gamma_2, mask = np.isnan(gamma_2))
                 gamma_2_av[measured_resnums] = np.ma.average(gamma_2, axis=0).data
-
                 all_gamma_2.append(gamma_2_av)
+
             y = np.mean(all_gamma_2,axis=0)
             mask = np.isfinite(y)&np.isfinite(prot.expPREs.value[label].values)
             chi = (prot.expPREs.value[label].values[mask] - y[mask]) / prot.expPREs.error[label].values[mask]
@@ -124,6 +126,7 @@ def optTauC(prot,model,chi2_results,rs_results,Nreplicas,tau_c_range):
             np.savetxt(prot.path+model+'/run{:d}/calcPREs/resBA-{:d}.dat'.format(run,label),np.c_[x,i_ratio,gamma_2_av])
             all_i_ratio.append(i_ratio)
             all_gamma_2.append(gamma_2_av)
+
         np.savetxt(prot.path+model+'/res-{:d}.dat'.format(label),np.c_[x,np.mean(all_i_ratio,axis=0),np.mean(all_gamma_2,axis=0),np.std(all_gamma_2,axis=0)/np.sqrt(Nreplicas)])
 
     return tc_min, calcChi2(prot,model)
@@ -131,12 +134,12 @@ def optTauC(prot,model,chi2_results,rs_results,Nreplicas,tau_c_range):
 for name in proteins.index:
     proteins.at[name,'expPREs'] = loadExpPREs(name,proteins.loc[name])
 
-tau_c_range = np.arange(args.tcmin,20.05,1)
+tau_c_range = np.arange(args.tcmin,8.05,1)
 
 chi2_results = pd.DataFrame(index=tau_c_range,
-                        columns=['{:.2f}'.format(i) for i in [1.00,1.06,1.08]])
+                        columns=['{:.2f}'.format(i) for i in [1.00,1.10,1.12]])
 rs_results = pd.DataFrame(index=tau_c_range,
-                        columns=['{:.2f}'.format(i) for i in [1.00,1.06,1.08]])
+                        columns=['{:.2f}'.format(i) for i in [1.00,1.10,1.12]])
 PREs = {}
 Nreplicas = 10
 # run PRE calculations
@@ -146,5 +149,5 @@ for i in [1.00,1.10,1.12]:
     tau_c, chi2_pre = optTauC(proteins.loc[args.name],model,chi2_results,rs_results,Nreplicas,tau_c_range)
     PREs[model] = {'chi2':chi2_pre, 'tau_c':tau_c, 'expPREs':proteins.at[args.name,'expPREs'], 'calcPREs':loadInitPREs(args.name,proteins.loc[args.name],model)}
     print(args.name,model,tau_c)
-pd.DataFrame(PREs).to_pickle('{:s}_PREs.pkl'.format(args.name))
+pd.DataFrame(PREs).to_pickle('{:s}_PREs_tc_opt.pkl'.format(args.name))
 print('Timing {:.3f}'.format(time.time()-time0))
